b4ac00bf6624e103c92b688786e69d317b300e12
Marek Dajnowski <marek@dajnowski.net>
2013-02-08 10:08:41 +0100

    Initial

--- README.md
+++ README.md
@@ -2,3 +2,43 @@ style-as-you-go
 ===============
 
 Textarea to write css which is instantly applied to page. Handy for fron-end web developers.
+
+License:
+========
+
+MIT
+http://en.wikipedia.org/wiki/MIT_License
+
+Installation:
+=============
+
+Put these into your header:
+<link href="/resources/css/style-as-you-go.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="/resources/js/style-as-you-go.js"></script>
+
+Requires jquery & jquery-ui so if you don't have them included in your project add these lines above style-as-you-go.js
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js"></script>
+
+Usage:
+======
+
+When page is loaded you will notice 'Style As You Go' button in the bottom right corener of page. Click it and you can see edit window.
+
+Type your css there, your browser will apply it instantly.
+
+If 'Pre-selector' field is filled, this selector will be applied before any rule you write. Example:
+In editor:
+p { color: red; }
+a { color: blue; }
+
+In 'Pre-selector' you will put .post
+
+Your browser will get:
+.post p { color: red }
+.post a { color: blue }
+
+IMPORTANT:
+==========
+
+Your styles are not saved anywhere and will dissapear if page is reloaded. When you are happy with your styling, copy them into your stylesheet file ;)
---
+++ style-as-you-go.js
@@ -0,0 +1,88 @@
+var StyleAsYouGo = function()
+{
+	this.contents = '';
+	this.pre_selector = '';
+	var _base = this;
+
+	this.initiate = function()
+	{
+		var handle_html = $( '<div class="style-as-you-go-button">Style As You go</div>' );
+		this.editor = $( '<div class="style-as-you-go-editor"><div class="move-handle">Move</div><div class="">Pre-selector <input type="text" name="style-as-you-go-pre-selector" value="" /></div> <textarea name="style-as-you-go-css" cols="40" rows="20"></textarea> </div>' );
+		this.style_element = $( "<style type=\"text/css\" id=\"style-as-you-go\">\n</style>" );
+
+		$( 'body' ).append( handle_html );
+		$( 'body' ).append( this.editor );
+		$( 'head' ).append( this.style_element );
+
+		$( this.editor ).draggable({
+			handle: '.move-handle'
+		});
+
+		$( handle_html ).click(function(){
+			$( _base.editor ).toggle();
+		});
+
+		this.editor.find('input[type=text]').change(function(){
+			_base.pre_selector = $( this ).val();
+			contents = _base.parseCss( this.contents );
+			$('#style-as-you-go').html( contents );
+		});
+
+		this.editor.find('textarea').keyup(function(){
+			this.contents = $( this ).val();
+			contents = _base.parseCss( this.contents );
+			$('#style-as-you-go').html( contents );
+		});
+	}
+
+	this.parseCss = function( contents )
+	{
+		if( !contents )
+		{
+			return;
+		}
+
+		var elements = [];
+		var new_css = '';
+		var declarations = contents.split( '}' );
+
+		for( i in declarations )
+		{
+			if( declarations[ i ].length > 0 && typeof( declarations[ i ] ) == 'string' )
+			{
+				var line = declarations[ i ];
+				var declaration = line.split( '{' );
+
+				if( declaration[ 0 ].length > 0 && typeof( declaration[ 0 ] ) == 'string' )
+				{
+					var elements = declaration[ 0 ].split( ',' );
+
+					var new_elements = [];
+
+					for( e in elements )
+					{
+						if( elements[ e ].length > 0 && typeof( elements[ e ] ) == 'string' )
+						{
+							var pre_selector = _base.pre_selector.length > 0 ? _base.pre_selector +' ' : '';
+							new_elements.push(  pre_selector + $.trim( elements[ e ] ) );
+						}
+					}
+
+					if( new_elements.length > 0 )
+					{
+						new_css += "\n"+ new_elements.join( ', ' ) + " {\n"+ $.trim( declaration[ 1 ] ) + "\n }";
+					}
+				}
+			}
+
+		}
+
+		return new_css;
+	}
+
+	this.initiate();
+}
+
+$(document).ready(function(){
+	var style_as_you_go = new StyleAsYouGo();
+});
